ff3ccd51d6202f22c99643c5bde973793a215eee
jake Molly <74493337+2412322029@users.noreply.github.com>
2022-09-29 20:31:27 +0800

    添加recyclerView,更新图标

--- app/src/main/java/com/example/applicationtest/TwoActivity.java
+++ app/src/main/java/com/example/applicationtest/TwoActivity.java
@@ -4,48 +4,32 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextClock;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.VideoView;
-
 import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.IOException;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 
 public class TwoActivity extends AppCompatActivity {
 
 
-    public String url= "http://127.0.0.1:5000/all";
+    public String url = "https://cdn.unrun.top/blog/output";
     public String data = null;
-    public String syncGet() {
+//    OkHttpClient client = new OkHttpClient().newBuilder().build();
+//    Request request = new Request.Builder().url(url).build();
+//    Response response = client.newCall(request).execute();
+//    ResponseBody responseBody = response.body();
+//    Log.d("http",responseBody);
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Response response = client.newCall(request).execute();
-                    data = response.body().string();
-                    Log.d("q",data);
 
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        return data;
+    public TwoActivity() throws IOException {
     }
-
-
 
 
     @Override
@@ -53,26 +37,19 @@ public class TwoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.twolayout);
 
-        //https://cdn.unrun.top/blog/v/UMP45xUMP9.mp4
-//        VideoView videoView = findViewById(R.id.videoView);
-//        videoView.setVideoURI(Uri.parse("https://cdn.unrun.top/blog/v/UMP45xUMP9.mp4"));
-//        videoView.start();
-//        videoView.requestFocus();
 
-        Button button = findViewById(R.id.button_two1);
+        Button button = findViewById(R.id.btn_yes);
         TextView textView = findViewById(R.id.textView_two1);
+        EditText editText=findViewById(R.id.etext1);
 
-        textView.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data=syncGet();
-                textView.setText(data);
+                String ss =editText.getText().toString();
+                textView.setText(ss);
             }
         });
     }
-
-
-
 
 
 }
