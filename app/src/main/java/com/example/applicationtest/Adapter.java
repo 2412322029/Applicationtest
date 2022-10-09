0d1a3048efbb884aaac13b64c5c2fae5e34bdf54
jake Molly <74493337+2412322029@users.noreply.github.com>
2022-10-09 15:35:43 +0800

    列表删除功能(不流畅)

--- app/src/main/java/com/example/applicationtest/Adapter.java
+++ app/src/main/java/com/example/applicationtest/Adapter.java
@@ -1,24 +1,25 @@
 package com.example.applicationtest;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import java.util.List;
-
 
 public class Adapter extends RecyclerView.Adapter<Adapter.Myholder> {
     private View itemview;
@@ -65,24 +66,72 @@ public class Adapter extends RecyclerView.Adapter<Adapter.Myholder> {
         return holder;
     }
 
+    @SuppressLint({"SetTextI18n", "ClickableViewAccessibility"})
     @Override
     public void onBindViewHolder(@NonNull Myholder holder, @SuppressLint("RecyclerView") int position) {
         holder.textView1.setText("" + position);//转字符串
         holder.textView2.setText(list.get(position));
         holder.imageView_item1.setImageResource(addimg(position));
-        holder.linearLayout_item1.setOnClickListener(new View.OnClickListener() {
+//        holder.linearLayout_item1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //Toast.makeText(itemview.getContext(), position + "号:" + list.get(position), Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(holder.itemView.getContext(), MainActivity2.class);
+//                intent.putExtra("msg","position:"+position+"\n"+list.get(position));
+//                context.startActivity(intent);
+//                //onSwiped(position);
+//            }
+//        });
+        holder.linearLayout_item1.setOnTouchListener(new View.OnTouchListener() {
+            private int X;
+            private int lastX;
+            private int Y;
+            @SuppressLint("ResourceAsColor")
             @Override
-            public void onClick(View view) {
-                //Toast.makeText(itemview.getContext(), position + "号:" + list.get(position), Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(holder.itemView.getContext(), MainActivity2.class);
-                intent.putExtra("msg","position:"+position+"\n"+list.get(position));
-                context.startActivity(intent);
-
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                    case MotionEvent.ACTION_DOWN://当屏幕检测到第一个触点按下之后就会触发到这个事件。
+                        lastX = (int) event.getRawX();
+                        Y = (int) event.getRawY();
+                        X = (int) event.getRawX();
+                        break;
+                    case MotionEvent.ACTION_UP://当触点松开时被触发。
+                        if (Math.abs((int) event.getRawX() - X)<2){//单击，点击坐标没发生变化
+                            Intent intent = new Intent(holder.itemView.getContext(), MainActivity2.class);
+                            intent.putExtra("msg","position:"+position+"\n"+list.get(position));
+                            context.startActivity(intent);
+                        }
+                        if(Math.abs(((int) event.getRawX() - X))<200){
+                            holder.linearLayout_item1.setLeft(0);
+                        }else{
+                            onSwiped(position);
+                            holder.linearLayout_item1.setLeft(0);
+                        }
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        int dx = (int) event.getRawX() - lastX;
+                        int left = view.getLeft() + dx;
+                        holder.linearLayout_item1.setLeft(left);
+                        lastX = (int) event.getRawX();
+                        break;
+                }
+                return true;
             }
         });
 
 
     }
+
+
+
+    //  删除数据
+    @SuppressLint("NotifyDataSetChanged")
+    public void onSwiped(int p) {
+        list.remove(p);
+        notifyItemRemoved(p);
+        notifyDataSetChanged();
+    }
+
 
     @Override
     public int getItemCount() {
